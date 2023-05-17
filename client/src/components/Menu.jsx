618c3b9df959db1a13997c3d0ad8a62e2cafcce0
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2023-05-16 23:10:50 -0600

    creating dynamic menu option list

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,5 +1,46 @@
+import { useState } from 'react';
+import '../styles/Menu.scss';
+import classNames from "classnames";
+import { menuStates } from '../utils/MenuStates';
+
 const Menu = () => {
-    return <div></div>
+
+    const [ selectedOption, setSelectedOption ] = useState(menuStates[0]);
+    
+    const clickedOption = value => setSelectedOption(value);
+
+    return (
+        <div className='menu'>
+            <div className='menu__container'>
+                <div className='menu__container-options'>
+                    <ul className='menu__container-options-list'>
+                        <li>
+                            <h2 className='menu__container-options-list--header'>Menú</h2>
+                            <hr className='menu__container-options-divider'></hr>
+                        </li>
+                        {
+                            menuStates.map(menu => {
+                                const { label, value } = menu;
+                                return (
+                                    <li
+                                        className={classNames({
+                                            'menu__container-options-list--item': true,
+                                            'menu__container-options-list--selected': selectedOption.value === value, 
+                                        })}
+                                        key={value}
+                                        onClick={() => clickedOption(menu)}>
+                                        {label}
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                    <div className='menu__container-plates'>
+                    </div>
+                </div>
+            </div>      
+        </div>
+    );
 }
 
 export default Menu;
---
+++ client/src/utils/MenuStates.js
@@ -0,0 +1,22 @@
+export const menuStates = [
+    {
+        label: 'Desayunos',
+        value: 'breakfast'
+    },
+    {
+        label: 'Comidas y Cenas',
+        value: 'dinners'
+    },
+    {
+        label: 'All Day',
+        value: 'allDay'
+    },
+    {
+        label: 'Postres',
+        value: 'dessert'
+    },
+    {
+        label: 'Cocktails & Drinks',
+        value: 'cocktails'
+    }
+];
