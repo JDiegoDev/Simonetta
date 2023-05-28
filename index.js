ea3f377d99c58a0dc89a28e9ebf8f4aaf29c8b9b
Juan Diego Gimenez Pena <juandiego.gimenez@globant.com>
2023-05-27 22:50:25 -0600

    adding public folder

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const port = process.env.PORT || 5000;
 
+
 //production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -18,21 +19,9 @@ app.get('/', (req, res) => {
   res.send({ home: '/' });
 })
 
-app.get('/api/testpdf', (req, res) => {
-  console.log('we got to the server!!  with: ', req.query);
-    const pdfFilepath = './files/test.pdf';
-    console.log('pdfFilepath', pdfFilepath);
-    const src = fs.createReadStream(pdfFilepath);
-
-    res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'inline; filename=test.pdf'
-    });
-
-    src.pipe(res);
-})
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static('public'));
 
 //start server
 app.listen(port, (req, res) => {
