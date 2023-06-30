f8974125b05298af3696cd24668d9f60a7401e7f
JDiegoDev <diego.gimenez.dev@gmail.com>
2023-06-30 14:06:45 -0600

    adding responsive images to file

--- client/src/utils/HomeImageSlides.js
+++ client/src/utils/HomeImageSlides.js
@@ -2,26 +2,31 @@ import Image1 from '../images/image1.png';
 import Image2 from '../images/image2.png';
 import Image3 from '../images/image3.png';
 import Image4 from '../images/image4.png';
+import ImageResponsive from '../images/image1-responsive.png';
 
 export const ImageData =  [
     {
         label: 'Image 1',
         alt: 'image1',
         src: Image1,
+        srcResponsive: ImageResponsive,
     },
-    {
+     {
         label: 'Image 2',
         alt: 'image2',
         src: Image2,
+        srcResponsive: ImageResponsive,
     },
     {
         label: 'Image 3',
         alt: 'image3',
         src: Image3,
+        srcResponsive: ImageResponsive,
     },
     {
         label: 'Image 4',
         alt: 'image4',
         src: Image4,
+        srcResponsive: ImageResponsive,
     }
 ];
