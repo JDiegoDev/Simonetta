e1fee9475fb34cfea4bf813c418be636865fcbf5
JDiegoDev <diego.gimenez.dev@gmail.com>
2023-11-07 19:29:48 -0600

    adding google login button

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,6 +10,10 @@ const Login = () => {
             </div>
             <div className="login-right-panel">
                 <h1>Inicio de Sesión</h1>
+                <a className='login-button-container' href="/auth/google">
+                    <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                    Inicio de Sesión con Google
+                </a>
             </div>
         </div>
     );
